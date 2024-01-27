b1c0a9179a8ace96cf0b0602a0807dc6517dcb97
Sanjay Devrari <143216721+Sanjay-Devrari@users.noreply.github.com>
2024-01-27 20:40:30 +0530

    Update Variables.js
    In this code we will talk about 3 (var,let,const) variables

--- Variables&Data/Variables.js
+++ Variables&Data/Variables.js
@@ -1,1 +1,14 @@
-
+// There are 3 types of Variable in Javascript.
+// (1) var :- Variable can be re-declared & updated.a global scope variable.
+var Name="Sanjay Devrari";
+var Name="Deepak Devrari";
+var Name="Ajay Devrari";
+console.log(Name);
+//(2) let :- Variable cannot be re-decalared but can be updated . a block scope variable.
+let Name="Sanjay Devrari";
+Name="Deepak Devrari";
+Name="Ajay Devrari";
+console.log(Name);
+//(3) const:- Variable cannot be re-decalared or updated . a block scope variable.
+const Water_bolling_point=100 C;
+console.log(Water_bolling_point);
