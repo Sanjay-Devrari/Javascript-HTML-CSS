54ff7231356fa47405c08d9854abf86ce9b33bee
Sanjay Devrari <143216721+Sanjay-Devrari@users.noreply.github.com>
2024-01-30 18:59:32 +0530

    Create For_and_While_loop.js

---
+++ Loops/For_and_While_loop.js
@@ -0,0 +1,16 @@
+//For Loop
+let num=5;
+for(let i=1;i<=10;i++)
+{
+    let result=num*i;
+    console.log(num,"X",i,"=",result);
+}
+//While Loop
+let i=1;
+while(i<=10)
+{
+    let res=num*i;
+    console.log(num,"X",i,"=",res)
+    i++;
+}
+ 
