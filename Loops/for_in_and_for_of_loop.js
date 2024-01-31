53a1a6e25f947ed3aa7f30027a6553e698b04a66
Sanjay Devrari <143216721+Sanjay-Devrari@users.noreply.github.com>
2024-01-31 20:46:47 +0530

    Create for_in_and_for_of_loop.js

---
+++ Loops/for_in_and_for_of_loop.js
@@ -0,0 +1,16 @@
+//For In loop
+let person={
+    name:"Sanjay",
+    work:"Collage Student",
+    age:21
+};
+for(let val in person)
+{
+    console.log(val);
+}
+//For of loop
+let arr=[1,4,5,6];
+for(let value of arr)
+{
+    console.log(value);
+}
